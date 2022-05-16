9bad42f5ca402595cdc4482cefe63a134e23c7cc
rayna016 <102683642+rayna016@users.noreply.github.com>
2022-05-15 23:05:46 -0500

    Add files via upload

---
+++ Final_Exam_ISI_CS_2022.js
@@ -0,0 +1,15 @@
+ 
+window.addEventListener("DOMContentLoaded", event => {
+  document.getElementById("my-final").innerHTML = "Hello ISI, I am done!"
+
+   
+  let div = document.querySelectorAll("div");
+  });
+
+  const div = document.getElementById('new-div');
+  div.insertAdjacentHTML(
+  'beforeend',
+  '<span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur quos qui tempore accusamus facilis porro fugiat quae ipsa temporibus possimus. Dicta impedit aliquam hic, accusamus beatae eos nam deleniti blanditiis?</span>',
+);
+
+  
